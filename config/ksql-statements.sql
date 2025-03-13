36bb055e7e9515d25ca97b3e5d171c4fa5a59010
Vincent Covelli <vincent.covelli78@gmail.com>
2025-03-13 02:10:27 -0400

    Added ksql to aggregate websocket data into OHLC for use in candle stick charting on the frontend and structured storage on the backend.

---
+++ config/ksql-statements.sql
@@ -0,0 +1,30 @@
+-- Create raw_trades stream
+CREATE STREAM raw_trades (
+    symbol STRING,
+    timestamp BIGINT,
+    price DOUBLE,
+    "size" BIGINT
+) 
+WITH (
+    KAFKA_TOPIC = 'stock-data',
+    VALUE_FORMAT = 'JSON'
+);
+
+-- Create the OHLC table using CTAS (symbol is the primary key automatically)
+CREATE TABLE ohlc_candles WITH (
+    KAFKA_TOPIC = 'ohlc-data',
+    VALUE_FORMAT = 'JSON',
+    PARTITIONS = 2,
+    REPLICAS = 1
+) AS
+    SELECT
+        symbol,  -- This becomes the PRIMARY KEY
+        WINDOWSTART AS bucket_start,
+        EARLIEST_BY_OFFSET(price) AS open,
+        MAX(price) AS high,
+        MIN(price) AS low,
+        LATEST_BY_OFFSET(price) AS close,
+        SUM("size") AS volume
+    FROM raw_trades
+    WINDOW TUMBLING (SIZE 1 MINUTE)
+    GROUP BY symbol;
